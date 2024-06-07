7d9fa7cb19a244613c3f6602af53b8755deb2cfc
Toshihide Imaruoka <imaru@neptune.kanazawa-it.ac.jp>
2024-06-07 12:23:59 +0900

    week9 update

--- week9_2024.R
+++ week9_2024.R
@@ -1,7 +1,11 @@
-d1<-rnorm(50, mean=43, sd=8)
-d2<-rnorm(50, mean=38, sd=6)
+ #d1<-rnorm(50, mean=43, sd=8)
+ #d2<-rnorm(50, mean=38, sd=8)
 
-d<-data.frame(cbind(d1,d2))
+ #d<-data.frame(cbind(d1,d2))
+ #saveRDS(d, file='week9d.obj')
+ 
+ d<-readRDS('week9d.obj')
+ 
 
 library(tidyverse)
 library(ggplot2)
@@ -15,3 +19,23 @@ mean(d1)
 sd(d1)
 mean(d2)
 sd(d2)
+
+p1<-rnorm(100000, mean=43, sd=8)
+p2<-rnorm(100000, mean=38, sd=8)
+p<-data.frame(cbind(p1,p2))
+lp<-pivot_longer(p,cols=c(p1,p2))
+g<-ggplot(data=lp, aes(fill=name, x=value))+geom_density(alpha=0.5)
+g<-g+labs(y='度数', x='正答数')
+plot(g)
+
+write.csv(d, file='week9data1.csv', row.names = F)
+
+t<-qt(0.05/2, length(d$d1)+length(d$d2)-2, lower.tail = F) 
+se<-sqrt((length(d$d1)*var(d$d1)*(length(d$d1)-1)/length(d$d1)+length(d$d2)*var(d$d2)*(length(d$d2)-1)/length(d$d2))/(length(d$d1)+length(d$d2)-2)*(1/length(d$d1)+1/length(d$d2)))
+upper<-(mean(d$d1)-mean(d$d2)) + t * se
+lower<-(mean(d$d1)-mean(d$d2)) - t * se
+
+
+#sp<-((length(d$d1)-1)*var(d$d1)+(length(d$d2)-1)*var(d$d2))/(length(d$d1)+length(d$d2)-2)
+
+#se2<-sqrt(sp/length(d$d1)+sp/length(d$d2))
